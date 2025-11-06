8cd72867baf0398ae1de3ef7acc682a864694c4c
Felipe <felipemarabotti989@gmail.com>
2025-11-06 20:40:38 -0300

    remove dotenv configuration and connection string from database setup

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {rejectUnauthorized: false}
 });
+
+pool.connect()
+  .then(() => console.log('Conectado ao PostgreSQL com sucesso!'))
+  .catch(err => console.error('Erro ao conectar:', err));
 
 module.exports = pool
